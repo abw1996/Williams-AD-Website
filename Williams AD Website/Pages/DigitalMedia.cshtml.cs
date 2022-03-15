221d3ee32d9856593e8351a649f9aa3cd9a317f3
Ashton Williams <ashtonwilliams1996@gmail.com>
2022-03-15 18:37:42 -0400

    Add project files.

---
+++ Williams AD Website/Pages/DigitalMedia.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Williams_AD_Website.Pages
+{
+    public class DigitalMediaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Williams AD Website/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+$(window).resize(function () {
+    var height = $("#navbar").height();
+    $(".height-set").height(height + 23);
+});
+
+$(document).ready(function () {
+    var height = $("#navbar").height();
+    $(".height-set").height(height + 23);
+});
